37fb385b019a50cea5e4109a2b45ee2f3b899119
TheAbzo <soon-ab@hotmail.com>
2019-05-06 23:30:04 +0200

    i have added those before (mux and ram)

--- code/generalComponent/2x1_MUX.vhd
+++ code/generalComponent/2x1_MUX.vhd
@@ -1,18 +1,18 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity MUX_2x1 is
+entity MUX2x1 is
     generic (n: integer := 16);
     port (
         inp1,inp2:in std_logic_vector (n-1 downto 0);
         selector:in std_logic;
         outp:out std_logic_vector (n-1 downto 0) 
   ) ;
-end MUX_2x1;
+end MUX2x1;
 
-architecture MUX_2x1Arch of MUX_2x1 is
+architecture MUX2x1Arch of MUX2x1 is
     begin
         outp <= inp1 when selector = '0'
         else inp2;
 
-end MUX_2x1Arch ; -- MUX_4x1Arch
+end MUX2x1Arch ; -- MUX_4x1Arch
