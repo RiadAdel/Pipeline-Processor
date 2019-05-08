efc473281842019ef0d656da680865138be7bd14
riad adel <riad.adel22@gmail.com>
2019-05-08 05:46:02 +0200

    edit excution

--- code/excute/ALU.vhd
+++ code/excute/ALU.vhd
@@ -1,6 +1,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use work.constants.all;
 
 entity ALU is
   generic(n: integer := 16);
@@ -36,10 +37,10 @@ begin
 
   -- adder
   op1 <= B;
-  op2 <= (one) when s = "0010" -- inc
-  else allOnes when s = "0011" -- dec
-  else A when s = "0100" -- add
-  else not(A) when s = "0101"
+  op2 <= (one) when s = ALUINC -- inc
+  else allOnes when s = ALUDEC -- dec
+  else A when s = ALUADD -- add
+  else not(A) when s = ALUSUB
   else allZeros;
 
   adderCarryIn <= '1' when s = "0101"
@@ -50,7 +51,7 @@ begin
   ----------------------------------------------------------------
 
   -- Shifter
-  direction <= '1' when s = "1001" -- if shift left
+  direction <= '1' when s = ALUSHR -- if shift right
   else '0';
 
   shifter: entity work.Shifter port map(direction,A(3 downto 0),B,shift,shifterCarryOut);
@@ -58,28 +59,28 @@ begin
 
   -- ALU outout
   outpu <= outp;
-  outp <= (others => '0') when s = "0000" -- reset
-  else not(B) when s = "0001" -- not
-  else adderOut when s = "0010" -- inc
-  else adderOut when s = "0011" -- dec
-  else adderOut when s = "0100" -- add
-  else adderOut when s = "0101" -- sub
-  else (A and B) when s = "0110" -- and
-  else (A or B) when s = "0111" -- or
-  else shift when s = "1000" -- shift left
-  else shift when s = "1001" -- shift right
-  else B when s = "1010" -- pass
-  else A when s = "1101" -- mov and in
+  outp <= (others => '0') when s = ALUFEQUAL0 -- reset
+  else not(B) when s = ALUNOT -- not
+  else adderOut when s = ALUINC -- inc
+  else adderOut when s = ALUDEC -- dec
+  else adderOut when s = ALUADD -- add
+  else adderOut when s = ALUSUB -- sub
+  else (A and B) when s = ALUAND -- and
+  else (A or B) when s = ALUOR -- or
+  else shift when s = ALUSHL -- shift left
+  else shift when s = ALUSHR -- shift right
+  else B when s = ALUFEQUALB -- pass
+  else A when s = ALUFEQUALA -- mov and in
   else (others => '0');
 
-  C <= shifterCarryOut when s = "1000" or s = "1001"
-  else adderCarryOut when s = "0010" or s = "0011" or s = "0100" or s = "0101"
-  else '1' when s ="1011" -- set carry
-  else '0' when s ="1100"; -- clear carry
+  C <= shifterCarryOut when s = ALUSHL or s = ALUSHR
+  else adderCarryOut when s = ALUINC or s = ALUDEC or s = ALUADD or s = ALUSUB
+  else '1' when s =ALUSETC -- set carry
+  else '0' when s =ALUCLEARC; -- clear carry
 
-  Z <= '1' when outp = "0000000000000000" and s /= "0000" and s /= "1010" and s /= "1101"
-  else '0' when s /= "0000" and s /= "1010" and s /= "1101";
+  Z <= '1' when outp = "0000000000000000" and s /= ALUFEQUAL0 and s /= ALUFEQUALB and s /= ALUFEQUALA 
+  else '0' when s /= ALUFEQUAL0 and s /= ALUFEQUALB and s /= ALUFEQUALA;
 
-  Ni <= '1' when outp(15) = '0' and s /= "0000" and s /= "1010" and s /= "1101"
-  else '0' when s /= "0000" and s /= "1010" and s /= "1101";
+  Ni <= '1' when outp(15) = '0' and s /= ALUFEQUAL0 and s /= ALUFEQUALB and s /= ALUFEQUALA
+  else '0' when s /= ALUFEQUAL0 and s /= ALUFEQUALB and s /= ALUFEQUALA;
 end ALUArch ;
